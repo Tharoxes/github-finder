8b34106fbc0291c6743b730d9832e33732d5f442
Thanh <thc.huynh@protonmail.com>
2022-09-23 01:29:18 +0200

    pushed fixes for tailwind and add pages

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Navbar from './components/layout/Navbar';
-import { useDebugValue } from 'react';
+import Footer from './components/layout/Footer';
 
+import About from './pages/About';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path='/' element={
-          <div className="bg-purple-500">
-            <h1 className='text-xl'>Hello World</h1>
-          </div>
-        }>
-        </Route>
-        <Route path='/' element={
-          <div className="flex flex-col justify-between h-screen">
-            <Navbar />
-            <main>Main Content</main>
-          </div>
-        }>
-        </Route>
-        <Route path='/' element={
-          <h1 className='text-xl'>Hello world</h1>
-        }>
-        </Route>
-      </Routes>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main className='container mx-auto px-3 pb-12'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/notfound' element={<NotFound />} />
+            <Route path='/*' element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </Router>
   );
 }
